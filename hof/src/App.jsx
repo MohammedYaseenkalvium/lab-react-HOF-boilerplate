d66f60871227d3599c437bae2665ddd4ac0d841c
Mohammed Yaseen <mohammed.yaseen@kalvium.community>
2024-05-06 15:08:53 +0530

    hof is done

---
+++ hof/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import HigherOrderComponent from "./Components/HigherOrderComponent";
+
+export default function App() {
+  return (
+    <>
+      <div className="App">
+        <HigherOrderComponent/>
+      </div>
+    </>
+  );
+}
